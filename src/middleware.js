10d105a59402afd0b461a92fde10d3c5d0f22a53
quangtudev <61562160+quangtudev@users.noreply.github.com>
2024-02-22 16:59:55 +0700

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,8 @@ import { NextResponse } from 'next/server';
 
 export function middleware(req) {
     let ua = req.headers.get('user-agent');
-    if(ua.includes('facebookexternalhit') || ua.includes('facebookcatalog')){
+    if(ua.includes('facebookexternalhit') || ua.includes('facebookcatalog') 
+        || ua.includes('Twitterbot') || ua.includes('twitterbot')){
         return NextResponse.next();
     } else {
         const targetURL = `${process.env.WORDPRESS_REDIRECT_DOMAIN}/${req.nextUrl.pathname.replace('/posts/', '')}`;
